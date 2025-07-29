1aff5ea3c2ba4932360250639199c8cb5f7f3a2f
João Araujo <juniormedeirosales@gmail.com>
2025-07-29 14:23:27 -0300

    added timer
    +added timer

---
+++ scenes/kitchen_minigame/scripts/TimerGenerator.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+@onready var progress_bar := $SubViewport/TextureProgressBar
+
+@export var total_time := 5.0
+var time_left := 0.0
+
+func _ready():
+	time_left = total_time
+	progress_bar.max_value = total_time
+	progress_bar.value = total_time
+
+func _process(delta):
+	if time_left > 0.0:
+		time_left -= delta
+		progress_bar.value = time_left
+	else:
+		progress_bar.value = 0
+		queue_free()  # ou hide()
