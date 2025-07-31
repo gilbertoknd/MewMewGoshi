c55696c8e16817294141c7191b59c1a14cd70e85
Gilbertoknd <gilbertoknd5@gmail.com>
2025-07-31 10:04:33 -0300

    NPC caminhando até a mesa funcional

--- scenes/kitchen_minigame/scripts/npc_cliente.gd
+++ scenes/kitchen_minigame/scripts/npc_cliente.gd
@@ -1,7 +1,49 @@
 extends CharacterBody3D
 
+'''
+Porta (9, -4)
+	
+Assentos, da esquerda para a direita
+(-5, -2) Assento 1
+(-2, -4) Assento 2
+(1, -4) Assento 3
+(4, -3) Assento 4
+
+Mesas na mesma ordem
+(-5, -3) Mesa 1
+(-2, -3) e (-1, -3) Mesa 2
+(2, -3) e (2, -4) Mesa 3
+(5, -3) Mesa 4
+
+Saindo da porta (9, -4)
+Pathing para o assento 1
+Para (-5, -2) : Caminhar até (4, -4) depois (4, -5) depois (-2, -5) depois (-4, -5) e (-4, -3), 
+Direction (-4, -3) - (-5, -3) = (1, 0) virar para a esquerda 
+
+Pathing para o assento 2
+Para (-2, -4) : Caminhar até (4, -4) depois (4, -5) depois (-2, -5) e (-2, -4)
+Direction (-2, -4) - (-2, -3) = (0, -1) virar para baixo
+
+Pathing para o assento 3
+Para (3, -4) : Caminhar até (3, -4)
+Direction (3, -4) - (2, -4) = (1, 0) virar para esquerda
+
+Pathing para o assento 4
+Para (4, -3) : Caminhar até (4, -4) depois (4, -3)
+Direction (4, -3) - (5, -3) = (-1, 0) virar para direita
+
+(0, 1) seria virar para cima
+'''
+
 @onready var sprite_visual = $NpcSprite3D
 @onready var pedido_icon = $NpcIconPedidoSprite3D
+
+@export var velocidade := 2.0
+
+var caminho_selecionado : Array
+var pedido_atual : Texture
+var indice_atual := 0
+var mesa_selecionada : Vector3
 
 #Skins aleatorias para os clientes, salvar em .tres para usar as animacoes
 var skins = [
@@ -16,65 +58,118 @@ var cardapio = [
 	"Sorvete"
 	]
 
-var posicoes_mesas: Dictionary = {
-	Vector3(-5, 1, -2) : 0,
-	Vector3(-2, 1, -4) : 0,
-	Vector3(1, 1, -4) : 0,
-	Vector3(4, 1, -3) : 0
+var caminhos_para_mesas: Dictionary = {
+	"assento_1": {
+		"mesa_coord": [Vector3(-5, 1, -3)],
+			
+		"path": [
+			Vector3(4, 1, -4),
+			Vector3(4, 1, -5),
+			Vector3(-2, 1, -5),
+			Vector3(-4, 1, -5),
+			Vector3(-4, 1, -3) #Assento_1_coord
+		],
+		"ocupado": 0,
+	},
+	"assento_2": {
+		"mesa_coord": [Vector3(-2, 1, -3)],
+			
+		"path": [
+			Vector3(4, 1, -4),
+			Vector3(4, 1, -5),
+			Vector3(-2, 1, -5),
+			Vector3(-2, 1, -4) #Assento_2_coord
+		],
+		"ocupado": 0,
+	},
+	
+	"assento_3": {
+		"mesa_coord": [Vector3(2, 1, -4)],
+			
+		"path": [
+			Vector3(3, 1, -4) #Assento_3_coord
+		],
+		"ocupado": 0,
+	},
+	
+	"assento_4": {
+		"mesa_coord": [Vector3(-5, 1, -3)],
+			
+		"path": [
+			Vector3(4, 1, -4),
+			Vector3(4, 1, -3) #Assento_4_coord
+		],
+		"ocupado": 0,
+	}
 }
 
-var mesa_selecionada : Vector3
-var pedido_atual: Texture
 
 func _ready() -> void:
-	global_position = Vector3(1, 1, -4)
-	
-	#Porta (9, -4)
-	
-	#Assentos, da esquerda para a direita
-	#(-5, -2) Assento 1
-	#(-2, -4) Assento 2
-	#(1, -4) Assento 3
-	#(4, -3) Assento 4
-	
-	#Mesas na mesma ordem
-	#(-5, -3) Mesa 1
-	#(-2, -3) e (-1, -3) Mesa 2
-	#(2, -3) e (2, -4) Mesa 3
-	#(5, -3) Mesa 4
-	
-	#Saindo da porta (9, -4)
-	#Pathing para o assento 1
-	#Para (1, -4) : Caminhar até (4, -4) depois (4, -5) depois (-2, -5) depois (-4, -5) e (-4, -3), 
-	#Direction (-4, -3) - (-5, -3) = (-1, 0) virar para a esquerda 
-	
-	#Pathing para o assento 2
-	#Para (1, -4) : Caminhar até (4, -4) depois (4, -5) depois (-2, -5) e (-2, -4)
-	#Direction (-2, -4) - (-2, -3) = (0, -1) virar para baixo
-	
-	#Pathing para o assento 3
-	#Para (1, -4) : Caminhar até (4, -4) depois (4, -5) depois (1, -5) e (1, -4)
-	#Direction (1, -4) - (2, -4) = (-1, 0) virar para a esquerda
-	
-	#Pathing para o assento 4
-	#Para (4, -3) : Caminhar até (4, -4) depois (4, -3)
-	#Direction (4, -3) - (5, -3) = (1, 0) virar para direita
-
-	
-
-	
+	global_position = Vector3(9, 1, -4)
 	#gerar_pedido()
 	aplicar_skin_aleatoria()
+	selecionar_mesa()
 	
 #Aleatorizando skin para o cliente	
 func aplicar_skin_aleatoria():
 	var skin_path = skins[randi() % skins.size()]
 	sprite_visual = skin_path
 	
-func checar_mesa_vazia():
-	var mesa_selecionada = posicoes_mesas[randi() % posicoes_mesas.size()]
+func selecionar_mesa():
+	var livres: Array = []
+
+	#Procurando assentos livres
+	for destino in caminhos_para_mesas:
+		var assento = caminhos_para_mesas[destino]
+		if assento["ocupado"] == 0:
+			livres.append(destino)
+	#
+	if not livres.is_empty():
+		randomize()
+		var escolha = livres[randi() % livres.size()]
+		caminho_selecionado = caminhos_para_mesas[escolha]["path"]
+		caminhos_para_mesas[escolha]["ocupado"] = 1
+		mesa_selecionada = caminhos_para_mesas[escolha]["mesa_coord"][0]
+
+func virar_para_mesa():
+	if caminho_selecionado.is_empty():
+		return
 	
+	var posicao_final = caminho_selecionado[-1]  #ultima posição do caminho (assento)
+	var mesa_pos = mesa_selecionada
 	
+	#Subtraindo e convertendo pra Vector2
+	var direcao = Vector2(
+		mesa_pos.x - posicao_final.x,
+		mesa_pos.z - posicao_final.z
+	).normalized()
+
+	#Decidir direção
+	if direcao == Vector2.LEFT:
+		$NpcSprite3D.animation = "walking_left"
+		$NpcSprite3D.stop()
+		$NpcSprite3D.frame = 0
+		
+		
+	elif direcao == Vector2.RIGHT:
+		$NpcSprite3D.animation = "walking_right"
+		$NpcSprite3D.stop()
+		$NpcSprite3D.frame = 0
+		
+	elif direcao == Vector2.UP:
+		$NpcSprite3D.animation = "walking_up"
+		$NpcSprite3D.stop()
+		$NpcSprite3D.frame = 0
+		
+	elif direcao == Vector2.DOWN:
+		$NpcSprite3D.animation = "walking_down"
+		$NpcSprite3D.frame = 0
+		$NpcSprite3D.playing = false
+		
+	else:
+		print("Direção inesperada:", direcao)
+
+		
 #Aleatorizando pedido dentro do vetor do cardapio, no script glogal GameManager
 func gerar_pedido():
 	var pedido_atual = load(cardapio[randi() % cardapio.size()])
@@ -85,20 +180,31 @@ func mostrar_pedido():
 	pedido_icon.billiboard_mode = BaseMaterial3D.BILLBOARD_ENABLED
 	
 func _physics_process(delta: float) -> void:
-	var direction = (mesa_selecionada - global_transform.origin)
-	direction.y = 1
+	if caminho_selecionado.is_empty() or indice_atual >= caminho_selecionado.size():
+		velocity = Vector3.ZERO
+		virar_para_mesa()
+		return
 	
-	if global_position == mesa_selecionada:
-		sprite_visual.play("idle_down")
-		mostrar_pedido()
-	
-	else:
-		move_and_slide()
+	var destino = caminho_selecionado[indice_atual]
+	var direction = destino - global_transform.origin
+	direction.y = 0  # manter no plano
 
-	#Mudar a direcao do sprite, mudando animacao
-	if abs(direction.x) > abs(direction.z):
-		if direction.x > 0: "walking_right"
-		else: "walking_left"
+	if direction.length() > 0.1:
+		direction = direction.normalized()
+		velocity = direction * velocidade
+		move_and_slide()
+		
+		# Atualiza animação com base na direção
+		if abs(direction.x) > abs(direction.z):
+			if direction.x > 0:
+				$NpcSprite3D.play("walking_right")
+			else:
+				$NpcSprite3D.play("walking_left")
+		else:
+			if direction.z > 0:
+				$NpcSprite3D.play("walking_down")
+			else:
+				$NpcSprite3D.play("walking_up")
+				
 	else:
-		if direction.z > 0: "walking_up"
-		else: "walking_down"
+		indice_atual += 1
