d41f89558ac24a8a88898ad15dcae9bb84a7d70a
Gilbertoknd <gilbertoknd5@gmail.com>
2025-08-02 11:04:15 -0300

    Fazendo pedido

--- scenes/kitchen_minigame/scripts/npc_cliente.gd
+++ scenes/kitchen_minigame/scripts/npc_cliente.gd
@@ -1,42 +1,9 @@
 extends CharacterBody3D
 
-'''
-Porta (9, -4)
-	
-Assentos, da esquerda para a direita
-(-5, -2) Assento 1
-(-2, -4) Assento 2
-(1, -4) Assento 3
-(4, -3) Assento 4
-
-Mesas na mesma ordem
-(-5, -3) Mesa 1
-(-2, -3) e (-1, -3) Mesa 2
-(2, -3) e (2, -4) Mesa 3
-(5, -3) Mesa 4
-
-Saindo da porta (9, -4)
-Pathing para o assento 1
-Para (-5, -2) : Caminhar até (4, -4) depois (4, -5) depois (-2, -5) depois (-4, -5) e (-4, -3), 
-Direction (-4, -3) - (-5, -3) = (1, 0) virar para a esquerda 
-
-Pathing para o assento 2
-Para (-2, -4) : Caminhar até (4, -4) depois (4, -5) depois (-2, -5) e (-2, -4)
-Direction (-2, -4) - (-2, -3) = (0, -1) virar para baixo
-
-Pathing para o assento 3
-Para (3, -4) : Caminhar até (3, -4)
-Direction (3, -4) - (2, -4) = (1, 0) virar para esquerda
-
-Pathing para o assento 4
-Para (4, -3) : Caminhar até (4, -4) depois (4, -3)
-Direction (4, -3) - (5, -3) = (-1, 0) virar para direita
-
-(0, 1) seria virar para cima
-'''
+#Porta (9, -4)
 
 @onready var sprite_visual = $NpcSprite3D
-@onready var pedido_icon = $NpcIconPedidoSprite3D
+@onready var pedido_icon = $NpcIconPedido
 
 @export var velocidade := 2.0
 
@@ -44,6 +11,13 @@ var caminho_selecionado : Array
 var pedido_atual : Texture
 var indice_atual := 0
 var mesa_selecionada : Vector3
+var chegou_na_mesa := false
+#Timers
+var tempo_de_escolha: Timer
+var tempo_de_espera_atendimento: Timer
+var tempo_de_espera_pela_comida: Timer
+var segundo_inicial = 3
+var segundo_final = 8
 
 #Skins aleatorias para os clientes, salvar em .tres para usar as animacoes
 var skins = [
@@ -51,11 +25,25 @@ var skins = [
 	#preload("res://scenes/kitchen_minigame/assets/sprites/clientes/npc_cliente_02.tres")
 ]
 
-#Substituir por sprites (path)
 var cardapio = [
-	"Cafe", 
-	"Bolo", 
-	"Sorvete"
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/001_coffe.png"), 
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/002_expresso.png"), 
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/17_burger_napkin.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/23_cheesecake_dish.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/27_chocolate_dish.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/35_donut_dish.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/39_friedegg_dish.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/43_eggtart_dish.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/45_frenchfries_dish.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/58_icecream_bowl.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/64_lemonpie_dish.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/87_ramen.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/93_sandwich_dish.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/98_sushi_dish.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/soft_drink_blue.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/soft_drink_green.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/soft_drink_red.png"),
+	preload("res://scenes/kitchen_minigame/assets/sprites/food/soft_drink_yellow.png")
 	]
 
 var caminhos_para_mesas: Dictionary = {
@@ -93,7 +81,7 @@ var caminhos_para_mesas: Dictionary = {
 	},
 	
 	"assento_4": {
-		"mesa_coord": [Vector3(-5, 1, -3)],
+		"mesa_coord": [Vector3(5, 1, -3)],
 			
 		"path": [
 			Vector3(4, 1, -4),
@@ -106,7 +94,6 @@ var caminhos_para_mesas: Dictionary = {
 
 func _ready() -> void:
 	global_position = Vector3(9, 1, -4)
-	#gerar_pedido()
 	aplicar_skin_aleatoria()
 	selecionar_mesa()
 	
@@ -150,7 +137,6 @@ func virar_para_mesa():
 		$NpcSprite3D.stop()
 		$NpcSprite3D.frame = 0
 		
-		
 	elif direcao == Vector2.RIGHT:
 		$NpcSprite3D.animation = "walking_right"
 		$NpcSprite3D.stop()
@@ -171,18 +157,32 @@ func virar_para_mesa():
 
 		
 #Aleatorizando pedido dentro do vetor do cardapio, no script glogal GameManager
-func gerar_pedido():
-	var pedido_atual = load(cardapio[randi() % cardapio.size()])
+func escolher_pedido():
+	var pedido_atual = cardapio[randi() % cardapio.size()]
+	tempo_de_escolha = Timer.new()
+	tempo_de_escolha.one_shot = true
+	tempo_de_escolha.wait_time = (randf_range(segundo_inicial, segundo_final))
+	tempo_de_escolha.timeout.connect(_on_tempo_de_escolha_timeout)
+	add_child(tempo_de_escolha)
+	tempo_de_escolha.start()
 
 func mostrar_pedido():
+	print("Entrou no mostrar pedido")
 	pedido_icon.texture = pedido_atual
 	pedido_icon.visible = true
-	pedido_icon.billiboard_mode = BaseMaterial3D.BILLBOARD_ENABLED
+
+func _on_tempo_de_escolha_timeout():
+	print("Entrou no timeout")
+	mostrar_pedido()
+
 	
 func _physics_process(delta: float) -> void:
-	if caminho_selecionado.is_empty() or indice_atual >= caminho_selecionado.size():
+	if indice_atual >= caminho_selecionado.size():
 		velocity = Vector3.ZERO
 		virar_para_mesa()
+		escolher_pedido()
+		chegou_na_mesa = true
+		caminho_selecionado = []
 		return
 	
 	var destino = caminho_selecionado[indice_atual]
