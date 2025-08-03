2c31fcd0ce66353491e792a08923ddf724e79a0e
João Araujo <juniormedeirosales@gmail.com>
2025-08-03 16:14:11 -0300

    added food interaction
    added food picking
    food delivery
    fixed some minor bugs

---
+++ scenes/kitchen_minigame/scripts/globals/Global_M1.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var currently_being_hold_food : FoodInstance = null
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func player_food_update() -> void:
+	pass
